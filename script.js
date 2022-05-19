da69a7eb7f0cc5fd57612dd2d4e7e5d7bd147100
jasmine1909 <jasminedrift@gmail.com>
2022-05-19 17:18:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ btnScroll.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+const sr = ScrollReveal({
+  distance: '45px',
+  duration: 2700,
+  reset: true
+})
+
+sr.reveal('.trend-item', { delay: 350, origin: 'left' })
+sr.reveal('.featured-item', { delay: 350, origin: 'right' })
+
+sr.reveal('.blog-item', ".footer-item", { delay: 200, origin: 'up' })
+
